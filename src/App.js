874c784d2f7ebb62ca3bf579ca64a9a474cc5399
shrihariburkul <shrihariburkul123@gmail.com>
2023-06-11 14:01:11 +0530

    done

--- src/App.js
+++ src/App.js
@@ -1,25 +1,44 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+   //import AddIcon from '@mui/icons-material/Add';
+  //import RemoveIcon from '@mui/icons-material/Remove';
+const App=()=>{
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const[num,setNum]=useState(0)
+const incNum=() => {
+setNum(num+1);
+};
+
+const decNum=()=>{
+if(num >0){
+  setNum(num-1)
 }
+else
+{
+  alert("limit reac to the ZERO")
+  setNum(0)
+}
+};
+return( 
+
+
+<> 
+<div className='main_div'>
+<div className='center_div'>
+
+<h1>  {num} </h1>
+
+<div className='btn_div'>
+  <button  onClick={incNum}> increm </button>
+                                               
+  <button onClick= {decNum}> decrem </button>
+                                              
+</div>
+  
+</div>
+    
+</div>
+</>
+)
+} 
 
 export default App;
